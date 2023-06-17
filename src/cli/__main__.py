735e676b41fb9e2f4ee0b70b86106a3788b6d506
Aurebeut <aurebeut@gmail.com>
2023-06-17 18:52:07 +0200

    add scaling and encoding

--- src/cli/__main__.py
+++ src/cli/__main__.py
@@ -95,17 +95,27 @@ def main(argv=sys.argv[1:]):
     X, y = shuffle(X, y, random_state=RANDOM_STATE)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = RANDOM_STATE)
 
+    #need to
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    X_train[cols_numerical] = scaler.fit_transform(X_train[cols_numerical])
+    X_test[cols_numerical] = scaler.transform(X_test[cols_numerical])
+
+    from sklearn.preprocessing import LabelEncoder
+    encoder = LabelEncoder()
+    X_train[cols_categorical] = X_train[cols_categorical].apply(lambda col: encoder.fit_transform(col))
+    X_test[cols_categorical] = X_test[cols_categorical].apply(lambda col: encoder.fit_transform(col))
+
     print("Training the models ...")
     #Display feature importance
     if args.algorithm_task == "classification":
         print("Classification")
-        y_pred, model = classif.logistic_regression(X,y)
-        #y_pred, model_result = classif.xgboost_classification(X,y)
-        #Save the model to a pickle file
-        print(y_train)
-        print(y_pred)
+        #y_pred, model = classif.logistic_regression(X_train,y_train,X_test)
+        y_pred, model = classif.xgboost_classification(X_train,y_train,X_test)
 
-        classif.model_evaluation(y_train,y_pred)
+        #Save the model to a pickle file
+
+        classif.model_evaluation(y_test,y_pred)
     #Give results and the "best" model selected
     if args.algorithm_task == "clustering":
         print("Clustering")
