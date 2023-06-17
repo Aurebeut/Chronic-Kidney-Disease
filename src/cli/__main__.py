c4af1d3a9d3096fa7470aad2bc098921d9873ebe
Aurebeut <aurebeut@gmail.com>
2023-06-17 18:08:22 +0200

    add cleaning target + classif algo

--- src/cli/__main__.py
+++ src/cli/__main__.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import sys
 from src.cli.dataprocessing import preprocess
+from src.cli.models import classif
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 RANDOM_STATE = 7
@@ -75,30 +76,39 @@ def main(argv=sys.argv[1:]):
 
     #preprocess variables
     df[target_col] = [preprocess.clean_variables(x) for x in df[target_col]]
+    for col in cols_of_variables :
+        df[col] = [preprocess.clean_variables(x) for x in df[col]]
+    df[target_col].replace(["ckd","notckd"],[1,0], inplace=True)
 
     #Split target variable and explaining variables
     X = df.drop([target_col,"Index_AD"], axis=1)
     y = df[target_col]
 
     #Impute the missing data
+    print("Imputing missing data")
     for col in cols_categorical:
         preprocess.impute_mode(X, col)
     for col in cols_numerical:
-        preprocess.impute_mean(y, col)
+        preprocess.impute_mean(X, col)
 
     #Scale/standardize before applying any model
     X, y = shuffle(X, y, random_state=RANDOM_STATE)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = RANDOM_STATE)
 
     print("Training the models ...")
-
-    #Train and run the models
-
-    #Save the model to a pickle file
-
     #Display feature importance
+    if args.algorithm_task == "classification":
+        print("Classification")
+        y_pred, model = classif.logistic_regression(X,y)
+        #y_pred, model_result = classif.xgboost_classification(X,y)
+        #Save the model to a pickle file
+        print(y_train)
+        print(y_pred)
 
+        classif.model_evaluation(y_train,y_pred)
     #Give results and the "best" model selected
+    if args.algorithm_task == "clustering":
+        print("Clustering")
 
 if __name__ == "__main__":
     main()
