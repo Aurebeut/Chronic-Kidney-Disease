22d91a52550cd2bbd3ee27fa4f98373f0336509b
Aurebeut <aurebeut@gmail.com>
2023-06-19 23:54:15 +0200

    clustering now functionnal

--- src/cli/__main__.py
+++ src/cli/__main__.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 import sys
 from src.cli.dataprocessing import preprocess
-from src.cli.models import classif
+from src.cli.models import classif, clustering
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -120,12 +120,11 @@ def main(argv=sys.argv[1:]):
     for col in cols_of_variables:
         df[col] = [preprocess.clean_variables(x) for x in df[col]]
     df[target_col].replace(["ckd", "notckd"], [1, 0], inplace=True)
+    y = df[target_col]
+    X = df.drop([target_col, "Index_AD"], axis=1)
 
     # Split target variable and explaining variables
     if args.algorithm_task == "classification":
-        X = df.drop([target_col, "Index_AD"], axis=1)
-        y = df[target_col]
-
         # Impute the missing data
         print("Imputing missing data")
         for col in cols_categorical:
@@ -133,18 +132,16 @@ def main(argv=sys.argv[1:]):
         for col in cols_numerical:
             preprocess.impute_mean(X, col)
 
-        # Scale/standardize before applying any model
+        # Shuffle and split the data before working on features and on model
         X, y = shuffle(X, y, random_state=RANDOM_STATE)
-
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.3, random_state=RANDOM_STATE
         )
-        print(type(X_train))
-        print(type(y_train))
-        # need to
-        scaler = StandardScaler()
-        X_train[cols_numerical] = scaler.fit_transform(X_train[cols_numerical])
-        X_test[cols_numerical] = scaler.transform(X_test[cols_numerical])
+
+        # Scale/standardize before applying any model
+        # scaler = StandardScaler()
+        # X_train[cols_numerical] = scaler.fit_transform(X_train[cols_numerical])
+        # X_test[cols_numerical] = scaler.transform(X_test[cols_numerical])
 
         encoder = LabelEncoder()
         X_train[cols_categorical] = X_train[cols_categorical].apply(
@@ -153,10 +150,34 @@ def main(argv=sys.argv[1:]):
         X_test[cols_categorical] = X_test[cols_categorical].apply(
             lambda col: encoder.fit_transform(col)
         )
-        print("Classification")
-        y_pred_logistic, model = classif.logistic_regression(X_train, y_train, X_test)
-        classif.model_evaluation(y_test, y_pred_logistic)
-        # Save the model to a pickle file
+
+        print("Classification model running ...")
+        import numpy as np
+        import statsmodels.api as sm
+
+        X_train = sm.add_constant(X_train)
+
+        # Fit the logistic regression model
+        logit_model = sm.Logit(y_train, X_train)
+        logit_result = logit_model.fit()
+
+        # Print model summary
+        print(logit_result.summary())
+
+        # Get the predicted probabilities for the test set
+        X_test = sm.add_constant(X_test)
+        y_pred_logistic = logit_result.predict(X_test)
+
+        # Convert predicted probabilities to class labels (0 or 1)
+        y_pred_logistic = np.where(y_pred_logistic >= 0.5, 1, 0)
+        print(y_pred_logistic)
+        # Compute accuracy score
+        accuracy = np.mean(y_pred_logistic == y_test)
+        print("Accuracy:", accuracy)
+
+        # y_pred_logistic, model = classif.logistic_regression(X_train, y_train, X_test)
+        # classif.model_evaluation(y_test, y_pred_logistic)
+        # # Save the model to a pickle file
 
         if args.save_model:
             filename = f"{args.algorithm_task}_model.pkl"
@@ -164,10 +185,8 @@ def main(argv=sys.argv[1:]):
             with open(f"{args.algorithm_task}_{date.today()}.pkl", "wb") as file:
                 pickle.dump(model, file)
 
-    # Give results and the "best" model selected
     elif args.algorithm_task == "clustering":
-        print("Clustering")
-        # As we want to identify the sub types ok CKD, we need to focus on CKD people only
+        # As we want to identify the sub types ok CKD, it's better focusing on CKD people only
         df_ckd = df[df[target_col] == 1]
         X = df_ckd.drop([target_col, "Index_AD"], axis=1)
 
@@ -178,34 +197,26 @@ def main(argv=sys.argv[1:]):
         for col in cols_numerical:
             preprocess.impute_mean(X, col)
 
-        # Preprocess numerical features (scale)
-        scaler = StandardScaler()
-        X[cols_numerical] = scaler.fit_transform(X[cols_numerical])
-        # scaled_numerical_features = scaler.fit_transform(df_ckd[])
-
         # Preprocess categorical features (one-hot encoding)
         encoder = LabelEncoder()
         X[cols_categorical] = X[cols_categorical].apply(
             lambda col: encoder.fit_transform(col)
         )
 
-        # Combine the scaled numerical features and encoded categorical features
-        # Apply K-means clustering
-        kmeans = KMeans(n_clusters=6, n_init="auto")
-        kmeans.fit(X)
+        # Helper to choose nb of clusters
+        clustering.k_means_elbow_curve(X, RANDOM_STATE)
 
-        # Get the cluster labels
-        cluster_labels = kmeans.labels_
-        cluster_labels_df = pd.DataFrame({"Cluster Labels": cluster_labels})
+        nb_clusters = int(
+            input(
+                "Considering the curve, please choose the number of clusters (it must be an integer) based on the elbow method, and then press enter : "
+            )
+        )
 
-        # Concatenate the df with the cluster labels
-        new_df = pd.concat([X, cluster_labels_df], axis=1)
-
-        # Compute the mean value of each column based on the cluster labels
-        # new_df[cols_numerical] = scaler.inverse_transform(X[cols_numerical])
-        # mean_values = new_df.groupby("Cluster Labels").mean()
-        # print(mean_values)
-
+        # Clustering part
+        results_df_signif = clustering.k_means_clustering(
+            X, cols_numerical, cols_categorical, nb_clusters, RANDOM_STATE
+        )
+        print(results_df_signif)
     else:
         print(
             "Please provide a correct argument for algorithm_task, either 'classification' or 'clustering'"
