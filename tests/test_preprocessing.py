8bd92167e5fe1855eb64f9624eb10f4398357a79
Aurebeut <aurebeut@gmail.com>
2023-06-19 14:58:48 +0200

    fix test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -3,18 +3,32 @@ import pandas as pd
 import numpy as np
 
 
+# def test_clean_variables():
+#     X = pd.DataFrame(
+#         {"Age": [10, 52, np.nan, "?"], "Name": ["aurelien", "? ", " c", " jack"]}
+#     )
+
+#     # Define expected output after preprocessing
+#     X_clean = pd.DataFrame(
+#         {"Age": [10.0, 52.0, np.nan, np.nan], "Name": ["aurelien", np.nan, "c", "jack"]}
+#     )
+
+#     for col in X.columns:
+#         X[col] = [preprocess.clean_variables(x) for x in X[col]]
+
+#     pd.testing.assert_frame_equal(preprocess.clean_variables(X), X_clean)
+
+
 def test_clean_variables():
-    X = pd.DataFrame({
-        'Age': [10, 52, np.nan, "?"],
-        'Name': ['aurelien', '? ', ' c', " jack"]
-    })
+    # Create a sample dataframe
+    data = {"col1": ["10", "?", " 10", "\t100"], "col2": ["  5  ", " ", "3 ", "\t"]}
+    df = pd.DataFrame(data)
 
-    # Define expected output after preprocessing
-    X_clean = pd.DataFrame({
-        'Age': [10, 52, np.nan, np.nan],
-        'Name': ['aurelien', np.nan, 'c', 'jack']
-    })
+    # Apply clean_variables function to each column
+    cols_of_variables = ["col1", "col2"]
+    for col in cols_of_variables:
+        df[col] = [preprocess.clean_variables(x) for x in df[col]]
 
-    preprocess.clean_variables(X)
-
-    pd.testing.assert_frame_equal(X, X_clean)
+    # Assert the expected results
+    assert df["col1"].tolist() == ["10", np.nan, "10", "100"]
+    assert df["col2"].tolist() == ["5", np.nan, "3", np.nan]
