480bc0e275b9631814deeba4bdce2d4adc9cc13f
Aurebeut <aurebeut@gmail.com>
2023-06-19 18:05:23 +0200

    add fct desc

--- src/cli/dataprocessing/preprocess.py
+++ src/cli/dataprocessing/preprocess.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 import pandas as pd
 
+
 def clean_variables(var):
     """Preprocess to handle missing values, ?, nan and variables with trailing tabs/spaces.
 
@@ -14,8 +15,8 @@ def clean_variables(var):
     var = str(var)
     if var is not None:
         var = var.strip()
-        var = re.sub(r"\t","", var)
-        var = re.sub(r"\?","", var)
+        var = re.sub(r"\t", "", var)
+        var = re.sub(r"\?", "", var)
         var = var.strip()
     else:
         var = np.nan
@@ -25,10 +26,29 @@ def clean_variables(var):
 
 
 def impute_mode(df, col_name):
+    """Impute missing values by their mode. Used for categorical variables
+
+    Args:
+        df (pd.DataFrame): dataframe containing data to impute
+        col_name (str): name of the column to clean
+
+    Returns:
+        df[col_name] (pd.Series): cleaned series
+    """
     df[col_name].fillna(df[col_name].mode()[0], inplace=True)
     return df[col_name]
 
+
 def impute_mean(df, col_name):
+    """Impute missing values by their mean. Used for numerical variables
+
+    Args:
+        df (pd.DataFrame): dataframe containing data to impute
+        col_name (str): name of the column to clean
+
+    Returns:
+        df[col_name] (pd.Series): cleaned series
+    """
     df[col_name] = df[col_name].apply(pd.to_numeric)
     df[col_name].fillna(df[col_name].mean(), inplace=True)
     return df[col_name]
