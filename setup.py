cdab0e098b3a77bc46e37e8b11f60ba6445864eb
Aurebeut <aurebeut@gmail.com>
2023-06-19 13:22:33 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -2,27 +2,24 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='chronic-kideny-disease',
-    author='Aurélien D.',
-    version='1.0.0',
+    name="chronic-kidney-disease",
+    author="Aurélien D.",
+    version="1.0.0",
     packages=find_packages(),
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     install_requires=[
-        'numpy~=1.23.3',
-        'pandas~=1.5.3',
-        'scikit-learn~=1.2.2',
-        ],
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Aurebeut/chronic-kidney-disease',
+        "numpy~=1.23.3",
+        "pandas~=1.5.3",
+        "scikit-learn~=1.2.2",
+    ],
     extras_require={
-        'notebook': [
-            'ipywidgets~=8.0.6',
-            'liac-arff~=2.5.0',
-            'openpyxl~=3.0.7',
-            'pandas~=1.5',
-            'scipy~=1.10.1',
-            'ydata-profiling~=4.2.0',
+        "notebook": [
+            "ipywidgets~=8.0.6",
+            "liac-arff~=2.5.0",
+            "openpyxl~=3.0.7",
+            "pandas~=1.5",
+            "scipy~=1.10.1",
+            "ydata-profiling~=4.2.0",
         ]
     },
 )
